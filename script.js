d3962265a3e72e94521dc944894f547b5952d0da
jbrown7407 <jbrown7407@gmail.com>
2020-12-12 16:11:13 -0500

    sidebar icon with styles

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= -1) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
