4b9328bced943af503fe7110a886d5aaf7e41800
jbrown7407 <jbrown7407@gmail.com>
2020-12-14 14:35:46 -0500

    prepping for deployment. grid changes still needed. CSS fixes for mobile first.

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+  function myFunction() {
+    navbar.classList.add("sticky")
   if (window.pageYOffset >= -1) {
     navbar.classList.add("sticky")
   } else {
